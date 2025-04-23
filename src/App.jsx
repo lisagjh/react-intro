b68005e695d0031262569766ad24544a3b0f0cb6
lisagjh <lisagjhoorn@outlook.com>
2025-04-23 16:28:59 +0200

    UPD

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import Board from './components/Board'
-import Game from './components/Game'
+import Board from "./components/Board";
+import Game from "./components/Game";
 
 function App() {
   return (
     <>
-      <Game />
+      <main>
+        {" "}
+        <Game />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
