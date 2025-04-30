2ba9e512eecec5dae37173504483a5f6544efdfb
lisagjh <lisagjhoorn@outlook.com>
2025-04-30 15:25:38 +0200

    finished task

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,6 +4,7 @@ import ToDoTask from "./ToDoTask";
 
 export default function ToDoList() {
   const [tasks, setTasks] = useState([]);
+  const [finishedTasks, setFinishedTasks] = useState([]);
 
   const addTask = (newTask) => {
     if (newTask.trim() !== "") {
@@ -11,15 +12,46 @@ export default function ToDoList() {
     }
   };
 
+  function finishTask(indexToRemove) {
+    // Get the task to move to finishedTasks
+    const taskToFinish = tasks[indexToRemove];
+
+    // Remove the task from tasks
+    const updatedTasks = tasks.filter((task, index) => index !== indexToRemove);
+
+    // Add the task to finishedTasks
+    setFinishedTasks([...finishedTasks, taskToFinish]);
+
+    // Update the tasks state
+    setTasks(updatedTasks);
+  }
+
   return (
     <>
       <h2>ToDo</h2>
 
       <ToDoForm onAddTask={addTask} />
 
+      <h3>Pending Tasks</h3>
       <ul className="todo__list">
         {tasks.map((task, index) => (
-          <ToDoTask key={index} task={task} />
+          <ToDoTask
+            key={index}
+            task={task}
+            onRemove={() => finishTask(index)}
+          />
+        ))}
+      </ul>
+
+      <h3>Finished Tasks</h3>
+      <ul className="todo__list">
+        {finishedTasks.map((task, index) => (
+          <ToDoTask
+            key={index}
+            task={task}
+            completed={true}
+            onRemove={() => finishTask(index)}
+          />
         ))}
       </ul>
     </>
--- src/components/ToDoTask.jsx
+++ src/components/ToDoTask.jsx
@@ -1,3 +1,16 @@
-export default function ToDoTask({ task }) {
-  return <li>{task}</li>;
+export default function ToDoTask({ task, onRemove, completed }) {
+  return (
+    <li
+      className={`todo__task ${
+        completed ? "todo__task__completed" : "todo__task"
+      }`}
+    >
+      {task}
+      <div className="buttons">
+        <button className="todo__task__button" onClick={onRemove}>
+          x
+        </button>
+      </div>
+    </li>
+  );
 }
